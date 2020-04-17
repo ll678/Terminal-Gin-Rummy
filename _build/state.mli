44c748efd05b83ae6f3cb0e2f0df32626abfd2cd
Lawrence Li <ll678@cornell.edu>
2020-04-17 17:06:46 -0400

    Added Makefile

---
+++ _build/state.mli
@@ -0,0 +1,10 @@
+(** 
+   Representation of dynamic game state.
+
+   This module represents the state of a game as it is being played,
+   including each player's hand and score, the cards in the discard pile, 
+   and the cards remaining in the deck.
+*)
+
+(** The abstract type of values representing the game state. *)
+type state 
