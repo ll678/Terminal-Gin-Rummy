7a6e65dcb6ff66fa2702b5fe2aacbcd35e798a73
Lawrence Li <ll678@cornell.edu>
2020-04-22 00:04:21 -0400

    Main module

--- main.ml
+++ main.ml
@@ -2,6 +2,14 @@ open Deck
 open Command
 open State
 
+(* Things to do:
+   Implement knocking in state
+   Implement getting current player's hand
+   Functions handling drawing and discarding produce results (Legal/Illegal) and not states
+   so pattern matching needs to be added to handle Legal of st and Illegal
+   If command is invalid, rerun previous state and prompt for command again
+*)
+
 let rec print_list lst =
   match lst with
   | [] -> ()
@@ -17,14 +25,36 @@ let rec play_game st =
   print_endline (List.nth (get_discard_pile st) 0);
 
   (* Print hand of current player 
-     (Function not yet defined in state.ml *)
-  print_list (get_current_player_hand);
+     (Function not yet defined in state.ml) *)
+  print_list (get_current_player_hand st);
 
   (* Prompt for player to draw. *)
   print_endline ("Draw a card from either the stock or the discard pile.");
+  match parse (read_line ()) with
+  | Draw deck -> let new_st = draw_deck deck st in
+    (* Print stock pile *)
+    print_list (current_stock_pile new_st);
 
+    (* Print first card in discard pile *)
+    print_endline (List.nth (get_discard_pile new_st) 0);
 
-  failwith "unimplemented"
+    (* Print hand of current player 
+       (Function not yet defined in state.ml) *)
+    print_list (get_current_player_hand new_st);
+    if knocking allowed then
+      (print_endline ("Discard a card from your hand or knock.");
+       match parse (read_line()) with
+       | Discard card -> let next_st = discard card new_st in play_game next_st
+       | Knock -> (* implement knocking function in state *) failwith "unimplemented"
+       | _ -> print_endline ("Invalid command."))
+    else 
+      (print_endline ("Discard a card from your hand or knock.");
+       match parse (read_line ()) with
+       | Discard card -> let next_st = discard card new_st in play_game next_st
+       | _ -> print_endline ("Invalid command."))
+  | _ -> print_endline ("Invalid command.")
+
+           failwith "unimplemented"
 
 (** [init_game n1 n2] starts a game of gin rummy with players [n1] and [n2]. *)
 let init_game name1 name2 =
